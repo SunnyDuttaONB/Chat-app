aad214b56f0007dba59a7488dd4e90da67be9efe
Sunny Dutta <sunnysanchiz2215@gmail.com>
2025-08-09 17:49:32 +0530

    Initial commit

---
+++ client/src/lib/utils.js
@@ -0,0 +1,12 @@
+export function formatMessageTime(date) {
+    if (!date) return "";
+
+    const parsedDate = new Date(date);
+    if (isNaN(parsedDate.getTime())) return "";
+
+    return parsedDate.toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+    });
+}
